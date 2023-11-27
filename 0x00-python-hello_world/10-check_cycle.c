d782ab53dcdb66e6414e482f50592a1a47d442c6
NORTH89 <nordintata0@gmail.com>
2023-11-27 21:48:45 +0100

    10-main.c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * check_cycle - Check if Has cycle or not
+ * @list: Linked List
+ * Return: result
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *slow, *fast;
+
+	if (!list)
+		return (0);
+	slow = list;
+	fast = list->next;
+	while (fast != NULL && fast->next != NULL)
+	{
+		if (slow == fast)
+			return (1);
+
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+
+	return (0);
+}
