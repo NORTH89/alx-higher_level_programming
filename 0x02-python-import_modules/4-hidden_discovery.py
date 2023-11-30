3dce02aed5e32c5ec2261927e513803d6b74c969
NORTH89 <nordintata0@gmail.com>
2023-11-30 21:00:07 +0100

    100-my_calculator.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import sys
-    print(sys.path)
-    from hidden_4 import hidden_4
+    """print all hidden directories"""
+    import hidden_4
     for name in dir(hidden_4):
         if not name.startswith("__"):
             print(name)
