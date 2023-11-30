30d1fa00138e04485c94284dd49335faa96f1c20
NORTH89 <nordintata0@gmail.com>
2023-11-30 13:45:15 +0100

    checks the number of command-line arguments passed to the script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 if __name__ == "__main__":
     import sys
     if len(sys.argv) - 1 == 0:
@@ -9,5 +9,7 @@ if __name__ == "__main__":
             print("1: {}".format(sys.argv[1]))
         else:
             print("{} arguments:".format(len(sys.argv) - 1))
-            for i in range(1, len(sys.argv)):
-                print("{}: {}".format(i, sys.argv[i]))
+            i = 0
+            while i < len(sys.argv) - 1:
+                print("{}: {}".format(i + 1, sys.argv[i + 1]))
+                i += 1
