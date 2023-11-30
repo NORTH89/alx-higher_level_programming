1a30f3107db67cfe11d0eae4f93f08f47365a412
NORTH89 <nordintata0@gmail.com>
2023-11-30 18:38:15 +0100

    variable loaded

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-    import hidden_4
+    import sys
+    print(sys.path)
+    from hidden_4 import hidden_4
     for name in dir(hidden_4):
         if not name.startswith("__"):
             print(name)
