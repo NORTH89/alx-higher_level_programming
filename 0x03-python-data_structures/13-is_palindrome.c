e283ac4e9e070e039ee42bce692fa3ca09dacf9d
NORTH89 <nordintata0@gmail.com>
2023-12-04 21:13:18 +0100

    function that adds 2 tuples

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,9 +1,11 @@
 #include "lists.h"
-/* BY CHARIFA MASBAHI*/
+
 /**
- * is_palindrome - checks if a singly linked list is a palindrome
- * @head: parameter
- * Return: result
+ * is_palindrome - checks if a singly linked list
+ * is a palindrome
+ * @head: pointer to head of list
+ *
+ * Return: 1 if palindrome, 0 if not
  */
 
 int is_palindrome(listint_t **head)
