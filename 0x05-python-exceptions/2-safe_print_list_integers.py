7b227e5563654e28f11d169b41dd2aa1da4b03a5
NORTH89 <nordintata0@gmail.com>
2023-12-18 18:40:25 +0100

    print list od integers

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    i = 0
+    try:
+        for j in range(x):
+            if isinstance(my_list[j], int):
+                print("{:d}".format(my_list[j]), end="")
+                i += 1
+        print()
+    except IndexError:
+        pass
+    return i
