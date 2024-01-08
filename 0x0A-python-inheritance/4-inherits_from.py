0a7948b04411d51c1cdfefeeba89a024b1717092
NORTH89 <nordintata0@gmail.com>
2024-01-08 22:31:43 +0100

    inherits class checking function

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" Defines an inherited class-checking function """
+""" function that returns True if the object is an instance of 
+	a class that inherited
+ """
 
 
 def inherits_from(obj, a_class):
