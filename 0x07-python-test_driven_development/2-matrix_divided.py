42daf38c4ad3d7f4be4502b9cc9e7a80dfd817a1
NORTH89 <nordintata0@gmail.com>
2024-01-08 18:40:30 +0100

    test my code

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python3
-matrix_divided = __import__("2-matrix_divided").matrix_divided
+""" Module for task 2 - matrix_divided """
 
-matrix = [[1, 2, 3], [4, 5, 6]]
-print(matrix_divided(matrix, 3))
-print(matrix)
+
+def matrix_divided(matrix, div):
+    """Divides all elements of a matrix"""
+
+    if not all(isinstance(row, list) for row in matrix) or not all(
+        isinstance(num, (int, float)) for row in matrix for num in row
+    ):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    if len(set(len(row) for row in matrix)) > 1:
+        raise TypeError("Each row of the matrix must have the same size")
+
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    new_matrix = [[round(num / div, 2) for num in row] for row in matrix]
+
+    return new_matrix
