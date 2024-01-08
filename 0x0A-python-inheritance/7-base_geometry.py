b037c9e9f8495073cc7ecd9e8e8008d319abf56c
NORTH89 <nordintata0@gmail.com>
2024-01-08 23:08:31 +0100

    Defines a class BaseGeometry

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
+""" Defines a class BaseGeometry. """
 
 
 class BaseGeometry:
+    """Defines a class BaseGeometry."""
+
     def area(self):
         raise Exception("area() is not implemented")
+
+    """ Raises an exception. """
 
     def integer_validator(self, name, value):
         if not isinstance(value, int):
