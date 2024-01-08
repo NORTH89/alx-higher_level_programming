a98307d920d473cd180aaea0ff64166466c3a184
NORTH89 <nordintata0@gmail.com>
2024-01-08 22:51:09 +0100

    Implement class BaseGeometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+class BaseGeometry:
+    """Defines a class BaseGeometry"""
+
+    def area(self):
+        """Not implemented"""
+        raise Exception("area() is not implemented")
