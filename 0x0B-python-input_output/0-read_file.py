6f6679417c6f3cfa47184a891c7eb839d79d2abc
NORTH89 <nordintata0@gmail.com>
2024-01-09 14:20:30 +0100

    function that reads a text file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""Module that reads a UTF8 text file and prints it to stdout"""
+"""reads a text file (UTF8) and prints it to stdout
+"""
 
 
 def read_file(filename=""):
-    """Function that reads a UTF8 text file and prints it to stdout"""
-
-    with open(filename, encoding="utf-8") as f:
-        print(f.read(), end="")
+    with open(filename, encoding="utf-8") as myFile:
+        print(myFile.read(), end='')
