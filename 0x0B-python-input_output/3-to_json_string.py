c4912469c37ffd42c33861ce19e8964eadb0ceac
NORTH89 <nordintata0@gmail.com>
2024-01-09 17:42:20 +0100

    Defines a function that returns the JSON representation of an object

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,9 +1,9 @@
 #!/bin/usr/python3
 """ Defines a function that returns the JSON representation of an object"""
 
+import json
+
 
 def to_json_string(my_obj):
-    import json
-
     """Returns the JSON representation of an object"""
     return json.dumps(my_obj)
