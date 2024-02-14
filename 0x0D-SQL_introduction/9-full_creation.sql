3bb1dd79bd11a3c06aee5d099ff4741a0cd906d6
NORTH89 <nordintata0@gmail.com>
2024-02-14 20:41:40 +0100

    creates a table second_table in the database

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,6 @@
 -- creates a table second_table in the database hbtn_0c_0 in MySQL server & add multiple rows
-INSERT INTO second_table (id, name, score) VALUES (5, "Jane", 7);
-INSERT INTO second_table (id, name, score) VALUES (6, "Sarah", 9);
-INSERT INTO second_table (id, name, score) VALUES (7, "Emily", 11);
-INSERT INTO second_table (id, name, score) VALUES (8, "Mary", 12);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
+INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 3);
+INSERT INTO second_table (id, name, score) VALUES (3, "Bob", 14);
+INSERT INTO second_table (id, name, score) VALUES (4, "George", 8);
