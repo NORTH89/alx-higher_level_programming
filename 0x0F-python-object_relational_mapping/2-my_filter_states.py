be80ba22ba82067153769b3d1f6908531471ef74
NORTH89 <nordintata0@gmail.com>
2024-03-16 02:06:45 +0000

    my filter states

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+script that lists all cities from the database hbtn_0e_4_usa
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "main":
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306,
+    )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'".format(sys.argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
