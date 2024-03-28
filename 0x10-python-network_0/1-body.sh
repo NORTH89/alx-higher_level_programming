a5df48c4165cca5f46761f8e50ac9686b3779ea5
NORTH89 <nordintata0@gmail.com>
2024-03-28 01:05:35 +0000

    script takes URL, and sends GET request and displays body

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Get the response body for a given URL for 200 status code responses.
+# Script takes URL, sends GET request and displays body of response
+curl -sL "$1"
 
-URL=$1
-curl -sL -o /dev/null -w "%{http_code}" "$URL" | grep -q "^200$" && curl -sL "$URL"
