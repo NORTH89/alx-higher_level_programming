720e3b925fba8febdd2233978ea145f88743b5e1
NORTH89 <nordintata0@gmail.com>
2024-03-29 01:25:54 +0000

    post email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
 """Sends a req to URL and display the res body.
 """
-import sys
-import urllib.error
+import urllib.parse
 import urllib.request
+import sys
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
 
-    request = urllib.request.Request(url)
-    try:
-        with urllib.request.urlopen(request) as response:
-            print(response.read().decode("ascii"))
-    except urllib.error.HTTPError as e:
-        print("Error code: {}".format(e.code))
+    request = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf8"))
