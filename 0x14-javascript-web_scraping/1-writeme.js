1f274a3df02d1e7050e6235263476ed9667079c1
NORTH89 <nordintata0@gmail.com>
2024-04-23 17:21:14 +0100

    script that writes a string to file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,9 +2,9 @@
 
 const fs = require("fs");
 
-fs.writeFile(process.argv[2], process.argv[3], "utf8",
-function (err) {
-  if (err) {
+fs.writeFile(process.argv[2], process.argv[3], 'utf8',
+  function (err) {
+   if (err) {
     console.log(err);
   }
 });
