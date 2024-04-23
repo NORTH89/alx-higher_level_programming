8cde28a7253c48c8197eb9df4e8321e8951a9925
NORTH89 <nordintata0@gmail.com>
2024-04-23 16:42:22 +0100

    README file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
+// Read from file
 
-const fs = require('fs');
-
-fs.writeFile(process.argv[2], process.argv[3], 'utf-8',
-  function (err) {
+const filesys = require('fs');
+filesys.readFile(process.argv[2], 'utf-8',
+  function (err, data) {
     if (err) {
       console.log(err);
+      return;
     }
+    console.log(data);
   });
