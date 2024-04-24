b72c8265a908edd04defbf413078ccc7393da831
NORTH89 <nordintata0@gmail.com>
2024-04-24 08:53:15 +0100

    script that prints all characters of a Star Wars movie

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,25 +1,30 @@
 #!/usr/bin/node
+import request from 'request';
 
-const request = require('request');
 const id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const content = JSON.parse(body);
-    const characters = content.characters;
-    // console.log(characters);
-    for (const character of characters) {
-      request.get(character, (error, response, body) => {
-        if (error) {
-          console.log(error);
-        } else {
-          const names = JSON.parse(body);
-          console.log(names.name);
-        }
-      });
-    }
+async function requestPromise(url) {
+  try {
+    const response = await request.get(url);
+    return response.body;
+  } catch (error) {
+    throw error;
   }
-});
+}
+
+async function printCharacters () {
+  try {
+    const body = await requestPromise(url);
+    const characters = JSON.parse(body).characters;
+
+    for (let i = 0; i < characters.length; i++) {
+      const characterBody = await requestPromise(characters[i]);
+      console.log(JSON.parse(characterBody).name);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+printCharacters();
